e4886ce2d552306b66f3ba578111801744623dd9
Sujal khatiwada <amarjack808@gmail.com>
2022-12-11 09:08:56 +0545

    add top nav html code

--- index.php
+++ index.php
@@ -7,6 +7,31 @@
     <title>Service finder | explore the best services/business around us</title>
 </head>
 <body>
+
+    <!-- Top section -->
+    <section>
+        <div class="container">
+            <!-- top nav -->
+            <div class="top-nav">
+                <!-- logo-container -->
+                <div class="logo-container">
+                    <h1>Find <span class="logo-color">Near Me</span></h1>
+                </div>
+                <!-- center-navigation -->
+                <div class="center-navigation">
+                    <a href="#">Home</a>
+                    <a href="#">Careers</a>
+                    <a href="#">Contact</a>
+                    <a href="#">About</a>
+                </div>
+                <!-- right-buttons -->
+                <div class="right-buttons">
+                    <a href="#">Log in</a>
+                    <a href="#">Register Now</a>
+                </div>
+            </div>
+        </div>
+    </section>
     
 </body>
 </html>
