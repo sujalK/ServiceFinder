6d75b1d3c46fc9b5200cbc4a81329c78f8416b4b
Sujal khatiwada <amarjack808@gmail.com>
2023-02-26 23:41:51 +0545

    make changes to the Validator class by adding extra get_email method and made method chainable

--- classes/Validator.class.php
+++ classes/Validator.class.php
@@ -1,21 +1,28 @@
 <?php 
 
 class Validator {
-    
+
     private string $email = '';
 
-    public function __construct($email = '')
+    public function __construct(string $email = '')
     {
         $this->set_email($email);
     }
 
-    private function set_email($email = ''): void {
+    private function set_email(string $email = ''): object 
+    {
         if ($this->validate_email($email)) {
             $this->email = $email;
         }
+        return $this;
+    }
+    
+    public function get_email(): string | NULL
+    {
+        return isset($this->email) ? $this->email : NULL;
     }
 
-    private function validate_email($email = ''): bool 
+    private function validate_email(string $email = ''): bool 
     {
        // Remove all illegal characters from email
         $email = filter_var($email, FILTER_SANITIZE_EMAIL);
